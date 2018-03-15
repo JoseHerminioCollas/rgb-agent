53a62267dda21299809208c94b592e2ebb4327b1
Jose Collas <joseherminiocollas@goatstone.com>
2018-03-15 11:01:42 -0700

    Add test for reset of engine

--- test/rx-engine.js
+++ test/rx-engine.js
@@ -1,19 +1,25 @@
-const expect = require('chai').expect
-const patterns = require('goatstone/rgb-agent/patterns/patterns')
-const EventEmitter = require('events').EventEmitter
 const Rx = require('rx')
+const chai = require('chai')
+const expect = chai.expect
+
+const EventEmitter = require('events').EventEmitter
+const td = require('testdouble')
+
+const patterns = require('goatstone/rgb-agent/patterns/patterns')
 const rXEngine = require('goatstone/rgb-agent/engine/rx-engine')
-var td = require('testdouble')
 
-    const scriptArray = patterns.emVehicle
+const scriptArray = patterns.emVehicle
 
-var startEvent = new EventEmitter()
+const startEvent = new EventEmitter()
 const startEffectEvent = new EventEmitter()
-let start$ = Rx.Observable.fromEvent(startEffectEvent, 'data')
-let stopEvent = new EventEmitter()
+const start$ = Rx.Observable.fromEvent(startEffectEvent, 'data')
+
+const stopEvent = new EventEmitter()
 const stop$ = Rx.Observable.fromEvent(stopEvent, 'data')
-let resetEvent = new EventEmitter()
+
+const resetEvent = new EventEmitter()
 const reset$ = Rx.Observable.fromEvent(resetEvent, 'data')
+
 let rXESub
 
 describe('RX Engine', () => {
@@ -21,13 +27,33 @@ describe('RX Engine', () => {
     beforeEach(() => {
         rXESub = rXEngine(start$, stop$, reset$, scriptArray, 1000)
     })
- 
-    it('should start and emit an object from array provided when started', ( ) => {
+
+    it('should start and emit an object from array provided when started', () => {
         rXESub.subscribe(data => {
             stopEvent.emit('data', 1)
             expect(data).to.deep.equal(scriptArray[0])
             done()
         })
-        startEffectEvent.emit('data'), 1
+        startEffectEvent.emit('data', 1)
     })
+
+    it('should reset to zero', done => {
+        let count
+        before(() => {
+            count = 0
+        })
+        rXESub.subscribe(data => {
+            // TODO confirm that this output increments, it is NOT 0
+        })
+        setTimeout(x => {
+            stopEvent.emit('data', 1)
+            rXESub.subscribe(data => {
+                expect(data).to.equal(0)
+            })
+            resetEvent.emit('data', 1)
+            done()
+        }, 10)
+        startEffectEvent.emit('data', 1)
+    })
+
 })
